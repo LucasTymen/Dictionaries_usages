d0493a79af1d27c6a929bf8c576e65c7160b616d
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-04 19:55:51 +0200

    08_items__Get_All_Items

--- 08_items__Get_All_Items/script.py
+++ 08_items__Get_All_Items/script.py
@@ -1,1 +1,5 @@
 pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+
+for role, rank in pct_women_in_occupation.items():
+  print("Women make up " + str(rank) + " percent of " + role + "s.")

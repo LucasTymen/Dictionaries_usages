29ad14a1b9f53d136e65266205ec9d4725553c16
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-04 14:05:34 +0200

    02_get_a_key

---
+++ 02_get_a_key/script.py
@@ -0,0 +1,1 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
--- 02_get_a_key/theory.py
+++ 02_get_a_key/theory.py
@@ -5,17 +5,16 @@ Get A Key
 
 Once you have a dictionary, you can access the values in it by providing the key. For example, let’s imagine we have a
 dictionary that maps buildings to their heights, in meters:
-
-building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World
-Tower": 554.5, "One World Trade": 541.3}
-
+"""
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+"""
 Then we can access the data in it like this:
-
->>> building_heights["Burj Khalifa"]
-828
->>> building_heights["Ping An"]
-599
-
+"""
+building_heights["Burj Khalifa"]
+# 828
+building_heights["Ping An"]
+# 599
+"""
 Instructions
 1.
 

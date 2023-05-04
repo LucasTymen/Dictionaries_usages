9359345c4c7b9634d03f2e2fb984083caf86b4bb
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-04 14:03:10 +0200

    02_get_a_key

---
+++ 02_get_a_key/theory.py
@@ -0,0 +1,30 @@
+"""
+
+Using Dictionaries
+Get A Key
+
+Once you have a dictionary, you can access the values in it by providing the key. For example, let’s imagine we have a
+dictionary that maps buildings to their heights, in meters:
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World
+Tower": 554.5, "One World Trade": 541.3}
+
+Then we can access the data in it like this:
+
+>>> building_heights["Burj Khalifa"]
+828
+>>> building_heights["Ping An"]
+599
+
+Instructions
+1.
+
+We have provided a dictionary that maps the elements of astrology to the zodiac signs. Print out the list of zodiac
+signs associated with the "earth" element.
+2.
+
+Print out the list of the "fire" signs.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+
+"""
